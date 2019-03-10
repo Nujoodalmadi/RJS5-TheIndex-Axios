82b12531fec6464ee2399872f937498c29f1c0c2
Nujood AlMadhi <nujoodalmadi@gmail.com>
2019-03-10 15:56:48 +0300

    submission

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
@@ -10,16 +9,41 @@ import AuthorDetail from "./AuthorDetail";
 class App extends Component {
   state = {
     currentAuthor: null,
-    filteredAuthors: authors
+    filteredAuthors: [],
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      const authors = response.data;
+      this.setState({
+        authors: authors,
+        filteredAuthors: authors,
+        loading: false
+      });
+    } catch (error) {
+      console.error("there is an error");
+      console.error(error);
+    }
+  }
+  selectAuthor = async author => {
+    this.setState({ loading: true });
+    const response = await axios.get(
+      "https://the-index-api.herokuapp.com/api/authors/" + author.id
+    );
+
+    this.setState({ currentAuthor: response.data, loading: false });
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -33,7 +57,7 @@ class App extends Component {
     } else {
       return (
         <AuthorsList
-          authors={this.state.filteredAuthors}
+          authors={this.state.authors}
           selectAuthor={this.selectAuthor}
           filterAuthors={this.filterAuthors}
         />
@@ -42,6 +66,8 @@ class App extends Component {
   };
 
   render() {
+    if (this.state.loading)
+      return <div className="content col-10">Loading</div>;
     return (
       <div id="app" className="container-fluid">
         <div className="row">
